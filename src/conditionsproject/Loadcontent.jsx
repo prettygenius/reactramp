e931d5177223d2dfdedd35725cfd3d7457d46a5c
prettygenius <devprettygenius@gmail.com>
2024-11-14 14:05:44 -0600

    correct url

--- src/conditionsproject/Loadcontent.jsx
+++ src/conditionsproject/Loadcontent.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-const url = 'https://api.github.com/users/prettygeniuss'
+const url = 'https://api.github.com/users/prettygenius'
 
 function Loadcontent() {
 
